bdde5b59af09efb90c68936f88b1a00f04424048
AndrewPao32 <andrewpaolini@gmail.com>
2022-04-02 18:26:14 -0700

    1 kata complete

--- CodeWars.js
+++ CodeWars.js
@@ -1,1 +1,10 @@
+// 1. Given a number n, return the number of positive odd numbers below n, EASY!
+function oddCount(n){
+    return Math.ceil((n-1)/2)
+}
+// Best Practices Answers
+const oddCount = n => Math.floor(n/2) 
 
+function oddCount(n){
+  return Math.floor(n/2);
+}
