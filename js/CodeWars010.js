8bdb0d3046b98947ac97fff6d8099bf3a4adf5da
AndrewPao32 <andrewpaolini@gmail.com>
2022-05-21 22:51:26 -0700

    Codewars 010

---
+++ js/CodeWars010.js
@@ -0,0 +1,23 @@
+/* Implement a function which multiplies two numbers.
+
+
+
+
+//Parameters: 
+
+//Returns: 
+
+//Examples:
+multiply(2, 3)
+
+//Pseudo Code:
+
+*/
+
+// Write here your multiply-function
+function multiply (a,b){
+  return a*b
+}
+
+
+
