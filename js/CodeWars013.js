fd02e4581570c2e72e4a42a102299124aa096ac1
AndrewPao32 <andrewpaolini@gmail.com>
2022-06-05 22:53:23 -0700

    Codewars 013

---
+++ js/CodeWars013.js
@@ -0,0 +1,26 @@
+/* Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+//Parameters: string
+
+//Returns: boolean
+
+//Examples:
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+//Pseudo Code:
+check if str endswith ending
+
+
+
+*/
+
+
+function solution(str, ending){
+  // TODO: complete
+  return str.endsWith(ending)
+
+}
+
+solution('abc','bc')  // true
+solution('abc','d') // false
